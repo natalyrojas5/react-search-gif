b6577dc50eb167478ceaa6499291bccc6f7b373e
Nataly Rojas <nataly@ludik.pe>
2020-08-16 01:25:02 -0500

    FEAT: BuscandoGIF-APP en producción

---
+++ docs/precache-manifest.664ca4dd291ff7c41da24e17c1aa0e23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c36abc14e33724edc2c5a07e8ca51b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4c01816037aa2517575",
+    "url": "/static/css/main.f887a094.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4c01816037aa2517575",
+    "url": "/static/js/main.d148c92a.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
